80eacadae68ebc7aad3576200dc34da50b82a61b
akbarismail <ismailakbar.me@gmail.com>
2022-12-14 17:37:59 +0700

    update pnpm to yarn and deploy with firebase

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = ''; // TODO: replace this
+const url = 'https://gallery-app-fae22.web.app'; // TODO: replace this
 const studentName = 'Akbar Ismail'; // TODO: replace this
 const studentId = 'FE2541268'; // TODO: replace this
 
